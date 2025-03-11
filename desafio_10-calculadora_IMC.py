eae43be715170fd90988f4ceb63c887dd9c8c743
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2025-03-11 12:51:36 -0300

    chore: adicionado funcao classifica IMC

--- desafio_10-calculadora_IMC.py
+++ desafio_10-calculadora_IMC.py
@@ -1,6 +1,23 @@
 #calculadora de IMC
 
+def classificaIMC(imc):
+    if imc < 18.5:
+        print('Abaixo do peso ideal!')
+    elif imc < 25.0:
+        print('Peso normal')
+    elif imc < 30.0:
+        print('Sobrepeso')
+    else:
+        print('Sobrepeso acentuado')
+
+
+#Bloco principal
 peso = float(input('Digite seu peso: '))
 altura = float(input('Digite sua altura: '))
 imc = peso / (altura**2)
-print(f'IMC = {imc:.2f}')
+classificaIMC(imc)
+
+
+
+
+
