e8cbc888217fff62653d523f74b62640deb6c258
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2025-03-11 13:46:07 -0300

    chore: adicionando repeticao de elementos com while

---
+++ desafio_14-100_palavras.py
@@ -0,0 +1,8 @@
+palavra = input('Digite uma palavra: ')
+
+contadorDePalavras = 1
+
+
+while contadorDePalavras <= 100:
+    print(palavra, contadorDePalavras)
+    contadorDePalavras = contadorDePalavras + 1
