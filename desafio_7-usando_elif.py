a911382099f62a0a3d29ff40d48f6498b1f6a422
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2025-03-11 12:10:54 -0300

    chore: adicionado exercicio utilizando elif

---
+++ desafio_7-usando_elif.py
@@ -0,0 +1,9 @@
+n1 = 10
+n2 = 15
+
+if n1 == n2:
+    print('Os valores são iguais')
+elif n1 > n2:
+    print(f'O valor de n1: {n1} é maior que o valor de n2: {n2}')
+else:
+    print(f'O valor de n2: {n2} é maior que o valor de n1: {n1}')
