990e9c0a3a9c7a6d6b69237b60cc333ed88b465d
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2025-03-15 16:04:33 -0300

    chore: desafio criar senha com funcoes

--- desafio-16_de_funcao.py
+++ desafio-16_de_funcao.py
@@ -1,6 +1,15 @@
-def somaAlgorismos(numero_dois_algorismos):
-    dezena = numero_dois_algorismos // 10
-    unidade = numero_dois_algorismos % 10
-    print(dezena + unidade) 
+def somaAlgorismos(n):
+    return n // 10 + n % 10
 
-somaAlgorismos(28)
+def criarSenha(dia,mes,ano):
+    parte1 = somaAlgorismos(dia)
+    parte2 = somaAlgorismos(mes)
+    parte3 = somaAlgorismos(ano % 100)
+    return str(parte1) + str(parte2) + str(parte3)
+
+#Bloco principal - testes
+
+print(somaAlgorismos(17))
+print(somaAlgorismos(9))
+print(criarSenha(17, 12, 1921))
+print(criarSenha(9, 5, 2024))
